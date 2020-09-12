e8d4b4c4cdfe3208923d32594d05bb48ed0a5a7a
Bolaji61 <balogunbolaji61@gmail.com>
2020-09-12 11:11:18 +0100

    Desired code output

--- app.py
+++ app.py
@@ -1,40 +1,48 @@
-from selenium import webdriver #Web scraping tool
+#web scraping library
+from selenium import webdriver
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait as wait
+import time
 
 options = webdriver.ChromeOptions()
 options.add_argument('--headless')
-options.add_argument('--disable-gpu')
+# options.add_argument('--disable-gpu')
 DRIVER_PATH = r"chromedriver"
 options.binary_location = r"/Applications/Google Chrome 3.app/Contents/MacOS/Google Chrome"
 driver = webdriver.Chrome(executable_path=DRIVER_PATH, options=options)
+time.sleep(3)
+driver.implicitly_wait(30)
+driver.maximize_window()
 
-url = "https://www.grubhub.com/restaurant/cast-iron-waffles-9604-longstone-ln-charlotte/1444707"  #URL
+#gspread
+import gspread
+
+url = 'https://www.grubhub.com/restaurant/cast-iron-waffles-9604-longstone-ln-charlotte/1444707'  #URL
 driver.get(url)
 
-def menuCategory():
-    """
-    Returns: A list of all categories of menu and their descriptions
-    """
-    categories = driver.find_elements_by_class_name("menuSection.navSection.undefined")
-    # print("Categories", categories)
-    sectionList_ = []
-    for category in categories:
-        sectionName = category.find_element_by_class_name("menuSection-headerTitle.u-flex.u-flex-justify-xs--between").text
-        sectionDescription = category.find_element_by_class_name("menuSection-desc.body.u-text-secondary").text
-        sectionList = [sectionName, sectionDescription]
-        sectionList_.append(sectionList)
-    return sectionList_
+gc = gspread.service_account(filename="./service_account.json")
+sh = gc.open("GrubHub Menu")
+worksheet = sh.sheet1
+worksheet.append_row(values=["TYPE", "NAME", "DESCRIPTION", "PRICE"]) #Append first header row
 
-def menuItems():
-    """
-    Returns: A list of all sub-category menu(Items), their descriptions and prices.
-    """
-    menuItems = driver.find_elements_by_class_name("menuItem.u-background--tinted.u-clickable.u-inset-1 ")
-    # print("Menu Items", menuItems)
-    itemList_ = []
+preorder = driver.find_element_by_css_selector('[class="ghs-preorderButton s-btn s-btn-primary"]')
+preorder.click()
+
+categories = driver.find_elements_by_class_name("menuSection.navSection.undefined")
+
+print(categories)
+for category in categories:
+    print(category)
+    sectionName = category.find_element_by_class_name("menuSection-headerTitle.u-flex.u-flex-justify-xs--between").text
+    sectionDescription = category.find_element_by_class_name("menuSection-desc.body.u-text-secondary").text
+    sectionList = ["Category", sectionName, sectionDescription]
+    worksheet.append_row(values=sectionList)  #append Category Names and description
+
+    menuItems = category.find_elements_by_class_name("menuItem.u-background--tinted.u-clickable.u-inset-1 ")
     for menuItem in menuItems:
         itemName = menuItem.find_element_by_class_name("menuItemNew-name").text
         itemPrice = menuItem.find_element_by_class_name("menuItem-displayPrice").text
         itemDescription = menuItem.find_element_by_class_name("u-text-secondary.menuItemNew-description--truncate").text
-        itemList = [itemName, itemDescription, itemPrice]
-        itemList_.append(itemList)
-    return itemList_
+        itemList = ["Item", itemName, itemDescription, itemPrice]
+        worksheet.append_row(values=itemList)   #append Item names, description and price
+        
